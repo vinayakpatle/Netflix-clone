0683f1a8036397ec3dabec121448a185646ff040
Vinayak patle <vinayakpatle2@gmail.com>
2025-04-16 15:18:29 +0530

    make sure up to date

---
+++ frontend/src/component/Navbar.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { LogOut, Menu, Search } from 'lucide-react'
+import { useAuthStore } from '../store/authStore'
+
+const Navbar = () => {
+    const [isMobileMenuOpen, setIsMbileMenuOpen] = useState(false);
+    const { logout, user } = useAuthStore()
+
+    const toggleMenuOpen = () => {
+        setIsMbileMenuOpen(curr => !curr);
+    }
+
+    return <>
+        <header className='max-w-6xl mx-auto flex flex-wrap items-center justify-between h-20 p-4'>
+            <div className='flex items-center gap-10 z-50'>
+                <Link to='/' >
+                    <img src='/netflix-logo.png' alt='Netflix-logo' className='w-32 sm:w-40 md:w-52' />
+                </Link>
+
+                {/* desktop navbar item */}
+                <div className='hidden sm:flex items-center gap-2'>
+                    <Link to='/' className='hover:underline'>
+                        Movies
+                    </Link>
+
+                    <Link to='/' className='hover:underline'>
+                        Tv Shows
+                    </Link>
+
+                    <Link to='/history' className='hover:underline'>
+                        Search History
+                    </Link>
+                </div>
+            </div>
+
+            <div className='flex items-center gap-2 z-50'>
+                <Link to='/search'>
+                    <Search className='size-6 cursor-pointer' />
+                </Link>
+                <img src={user.image} alt='avatar-logo' className='rounded cursor-pointer h-8' />
+                <LogOut className='size-6 cursor-pointer' onClick={logout} />
+                <div className='sm:hidden'>
+                    <Menu className='size-6 cursor-pointer' onClick={toggleMenuOpen} />
+                </div>
+            </div>
+
+            {/* mobile navbar item */}
+            {isMobileMenuOpen && (<div className='w-full sm:hidden mt-4 z-50 border border-gray-800 bg-black rounded'>
+                <Link to='/' className='p-2 block hover:underline' onClick={toggleMenuOpen} >
+                    Movies
+                </Link>
+
+                <Link to='/' className='p-2 block hover:underline' onClick={toggleMenuOpen}>
+                    Tv Shows
+                </Link>
+
+                <Link to='/history' className='p-2 block hover:underline' onClick={toggleMenuOpen}>
+                    Search History
+                </Link>
+            </div>)}
+        </header>
+
+
+    </>
+}
+
+export default Navbar
--- frontend/src/pages/Home/HomeScreen.jsx
+++ frontend/src/pages/Home/HomeScreen.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
 import { useAuthStore } from '../../store/authStore'
+import Navbar from '../../component/Navbar'
+import { Link } from 'react-router-dom'
+import {Play,Info} from 'lucide-react'
 
 const HomeScreen = () => {
-  const { logout } = useAuthStore()
+  
 
   return (
-    <div className='hero-bg'>
-        <button onClick={logout}>
-          Logout
-        </button>
+    <div className='relative h-screen text-white '>
+        <Navbar />
+        <img src='/extraction.jpg' alt='Content-poster' className='absolute top-0 left-0 w-full h-full object-cover -z-50'/>
+
+        <div className='absolute top-0 left-0 w-full h-full bg-black/50 -z-50' aria-hidden='true' />
+
+        <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32'>
+          <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black via-transparent to-transparent' />
+
+          <div className='max-w-2xl'>
+            <h1 className='text-6xl mt-4 font-extrabold text-balance'>
+              Extraction
+            </h1>
+            <p className='mt-2 text-lg'>
+              2020 | 18+
+            </p>
+            <p className='mt-4 text-lg'>
+            While battling his own demons, Tyler, a ruthless mercenary, accepts the offer of rescuing an Indian crime lord's teenage son from Bangladesh. However, when he arrives, a gruesome blood bath ensues.
+            </p>
+          </div>
+
+          <div className='mt-8 flex'>
+            <Link to='/play' className='flex items-center mr-4 bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded'>
+              <Play className='size-6 mr-2 fill-black' />
+              Play
+            </Link>
+
+            <Link to='/info' className='flex items-center bg-gray-500/30 hover:scale-105 text-white font-medium rounded py-2 px-4 '>
+              <Info className='size-6 mr-2' />
+              More Info
+            </Link>
+          </div>
+        </div>
     </div>
   )
 }
